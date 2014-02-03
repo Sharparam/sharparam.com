5d10b393350dfeb896d2e06dd608436c70cc47bc
Adam Hellberg <sharparam@sharparam.com>
2014-02-03 19:13:47 +0100

    Fix deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ namespace :deploy do
     %w[start stop restart].each do |command|
         desc "#{command} unicorn server"
         task command, roles: :app, except: {no_release: true} do
-            run "#{current_path}/config/unicorn_init.sh #{command}"
+            sudo "/etc/init.d/unicorn_#{application} #{command}"
         end
     end
 
