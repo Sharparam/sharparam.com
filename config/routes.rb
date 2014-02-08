72ae2a860946f5854333a66c7fc2aef57062a050
Adam Hellberg <sharparam@sharparam.com>
2014-02-08 14:41:04 +0100

    Modified blog route to be more logical

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,13 @@
 SharparamCom::Application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-  
-  #devise_scope :user do
-  #  get 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
-  #end
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
   root 'pages#index'
 
   resources :users
-  resources :posts
+  resources :blog, controller: 'posts', as: :posts
 
   get 'projects' => 'pages#projects'
   get 'about' => 'pages#about'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
 
   # Example resource route with options:
   #   resources :products do
