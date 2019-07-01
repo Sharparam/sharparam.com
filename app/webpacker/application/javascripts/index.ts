29349a98db042586c10bb1e6184a1aab8f13d6b3
Adam Hellberg <sharparam@sharparam.com>
2019-07-02 00:20:36 +0200

    Fix TypeScript issues

--- app/webpacker/application/javascripts/index.ts
+++ app/webpacker/application/javascripts/index.ts
@@ -1,6 +1,7 @@
-require('@rails/ujs').start();
-import Turbolinks from 'turbolinks';
+import * as RailsUjs from '@rails/ujs';
+import * as Turbolinks from 'turbolinks';
 import * as ActiveStorage from '@rails/activestorage'
 
+RailsUjs.start();
 Turbolinks.start();
 ActiveStorage.start();
