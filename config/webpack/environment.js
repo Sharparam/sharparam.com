8a2ac410796f118fbdfd526961a7836755aec284
Adam Hellberg <sharparam@sharparam.com>
2019-07-12 00:45:03 +0200

    Add purgecss

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,18 @@
 const { environment } = require('@rails/webpacker')
 const typescript =  require('./loaders/typescript')
 const erb =  require('./loaders/erb')
+const path = require('path')
+const PurgecssPlugin = require('purgecss-webpack-plugin')
+const glob = require('glob-all')
 
 environment.loaders.prepend('erb', erb)
 environment.loaders.prepend('typescript', typescript)
+
+environment.plugins.append('PurgecssPlugin', new PurgecssPlugin({
+  paths: glob.sync([
+    path.join(__dirname, '../../app/webpacker/**/*.js'),
+    path.join(__dirname, '../../app/views/**/*.slim')
+  ])
+}))
+
 module.exports = environment
